40d9edbed3a1c972182f131c7080c9acc315aada
Aegean <egeaybars1102@gmail.com>
2024-02-14 00:46:06 +0300

    hexlify to utf8

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const ethers = require("ethers")
 require("dotenv").config()
 
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL)
-const mnemonic = process.env.SEED_PHRASE
+const privateKey = process.env.PRIVATE_KEY
 const inscription = process.env.TEXT
 
-//Iterate the number "1" to get different wallet addresses.
-const derivationPath = "m/44'/60'/0'/0/" + 1
-const wallet = ethers.Wallet.fromPhrase(mnemonic, derivationPath).connect(provider)
+const wallet = new ethers.Wallet(privateKey, provider)
 
 async function sendTransaction(toAddress, data) {
     const transactionResponse = await wallet.sendTransaction({
         to: toAddress,
         value: 0,
-        data: ethers.encodeBytes32String(data)
+        data: ethers.hexlify(ethers.toUtf8Bytes(data))
       })
   
     console.log("Transaction sent:", transactionResponse.hash)
